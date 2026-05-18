ecec27679dd9718415090e95a75a4e90261f6e73
Paul-Christian Volkmer <code@pcvolkmer.de>
2026-05-18 12:53:38 +0200

    docs: change javadoc and comments

--- src/main/java/dev/pcvolkmer/mv64e/datamapper/mapper/TherapieplanDataMapper.java
+++ src/main/java/dev/pcvolkmer/mv64e/datamapper/mapper/TherapieplanDataMapper.java
@@ -118,12 +118,12 @@ public class TherapieplanDataMapper implements DataMapper<MtbCarePlan> {
     // Formularfeld "protokollauszug"
     var protokollauszug = therapieplanData.getString("protokollauszug");
     if (null != protokollauszug) {
-      // TODO see https://github.com/dnpm-dip/mtb-model/issues/8
       builder.notes(List.of(protokollauszug));
     }
 
     // Formularfeld "target"
     var target = therapieplanData.getString("target");
+    // Kein Target
     if ("KT".equals(target)) {
       builder.recommendationsMissingReason(getCarePlanRecommendationsMissingReasonCoding(target));
     }
